918f1d84abbd2808816b77d6b835cc300adae5d2
Antoine Cauchois <cauchois37@gmail.com>
2025-08-13 11:34:09 +0200

    exo

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,14 @@ Rails.application.routes.draw do
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
+  delete "tasks/:id", to: "tasks#destroy"
+  patch "tasks/:id", to: "tasks#update"
+  get "tasks/:id/edit", to: "tasks#edit", as: :edit_task
+  get "tasks/new", to: "tasks#new"
+  post "tasks", to: "tasks#create"
+  root to: "tasks#show"
+  get "/tasks/:id", to: "tasks#show", as: :task
+  get "tasks", to: "tasks#index"
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
